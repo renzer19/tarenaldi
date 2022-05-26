69abacb91f2a1c62a057e7e1292d006e14dec18b
renaldi <87884489+renzer19@users.noreply.github.com>
2022-05-27 06:06:03 +0700

    Update login_aksi.php

--- login_aksi.php
+++ login_aksi.php
@@ -18,7 +18,7 @@
     {
         $_SESSION['username']=$username;
         $_SESSION['status']='login';
-        header("location:admin/index.php");
+        header("location:admin/index.html");
     }
     else
     {
